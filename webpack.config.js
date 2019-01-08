579c148f858f34eec1aed1a56661bebc5ea202af
ukonpower <obshi0120@icloud.com>
2019-01-08 23:02:58 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const project = 'gl8';
+
+module.exports = {
+    mode: 'development',
+    entry: './src/' + project + '/js/main.js',
+    output:{
+		path: `${__dirname}/dist/gl/js/`,
+		publicPath: '/gl/js/',
+        filename: 'main.js'
+    },
+    devServer: {
+		contentBase: path.join(__dirname, '/dist/'),
+		openPage:"gl/",
+		compress: true,
+        open: true,
+		port: 9000,
+		host: '0.0.0.0',
+		disableHostCheck: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(glsl|vs|fs)$/,
+				loader: 'shader-loader',
+				options: {
+					glsl: {
+						chunkPath: path.resolve("/glsl/chunks")
+					}
+				}
+			}
+		]
+	}
+}
+
