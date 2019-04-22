8c44e2d706ce52b482df294f26b1574f8221a8b9
ukonpower <>
2019-04-22 15:10:03 +0900

    edit gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,25 +8,26 @@ import autoprefixer from 'gulp-autoprefixer';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-sass';
 import cssmin from 'gulp-cssmin';
+import pug from 'gulp-pug';
 import del from 'del';
 
 const options = minimist(process.argv.slice(2), {
-    default: { 
+    default: {
         gl: '1',
         P: false,
     }
 });
 
 const srcBase = './src/gl' + options.gl + "/";
-  
+
 gulp.task("webpack", () => {
     let conf = webpackConfig;
     conf.entry.main = srcBase + '/js/main.js';
     conf.output.filename = 'main.js';
 
-    if(options.P){
+    if (options.P) {
         conf.mode = 'production';
-    }else{
+    } else {
         conf.mode = 'development'
     }
 
@@ -35,6 +36,15 @@ gulp.task("webpack", () => {
         })
         .pipe(gulp.dest("./public/gl/js/"))
         .unpipe(browserSync.reload());
+});
+
+gulp.task('pug', () => {
+    return gulp.src(['./src/pug/**/*.pug', '!./src/pug/**/_*.pug'])
+        .pipe(plumber())
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./public/gl/'));
 });
 
 gulp.task("sass", () => {
@@ -48,13 +58,12 @@ gulp.task("sass", () => {
 });
 
 gulp.task('copy', (c) => {
-    gulp.src(['./src/html/**/*']).pipe(gulp.dest('./public/gl/'));
     gulp.src([srcBase + 'assets/**/*']).pipe(gulp.dest('./public/gl/assets/'));
     c();
 });
 
 gulp.task('browser-sync', () => {
-    
+
     browserSync.init({
         server: {
             baseDir: "public/",
@@ -71,21 +80,27 @@ gulp.task('reload', () => {
 gulp.task('clean', (c) => {
     del([
         './public/',
-    ]);
-    c();
+    ], {
+        force: true,
+    }).then(paths => {
+        // eslint-disable-next-line no-console
+        c();
+        console.log('Files and folders that would be deleted:\n', paths.join('\n'));
+    });
 });
 
 gulp.task('watch', () => {
     gulp.watch(srcBase + 'js/**/*', gulp.series('webpack'));
     gulp.watch('./src/scss/*.scss', gulp.task('sass'));
+    gulp.watch('./src/pug/**/*.pug', gulp.task('pug'));
     gulp.watch(srcBase + '**/*', gulp.task('copy'));
 });
 
 gulp.task('default', gulp.series(
     'clean',
-    gulp.parallel(
-        'webpack', 'sass'
-    ),
     'copy',
+    gulp.parallel(
+        'webpack', 'sass', 'pug'
+    ),
     gulp.parallel('browser-sync', 'watch'),
 ))
